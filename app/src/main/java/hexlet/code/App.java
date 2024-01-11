aa32e64057c26eb99fd3422fcd1480b1774b26e5
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-01-11 19:05:18 +0300

    меню 0-2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,24 @@
 package hexlet.code;
 
 import code.Cli;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli cli = new Cli();
-        cli.getName();
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+                Scanner scAnswer = new Scanner(System.in);
+        String answer = scAnswer.next();
+        System.out.println("Your choice: " + answer + "\n");
+        if (answer.equals("0")) {
+            System.out.println("See you next time!");
+        }
+        if (answer.equals("1")) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli cli = new Cli();
+            cli.getName();
+        }
+        else {
+            System.out.println("wrong");
+        }
     }
 }
