183272ed5b56ee5cda671a8e50f6a19459d0d683
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-01-24 00:17:49 +0300

    меню, 0-2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,34 @@
 package hexlet.code;
 
-import code.Cli;
+import games.Even;
+
+import java.util.Scanner;
+import hexlet.code.Cli;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli cli = new Cli();
-        cli.getName();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calculator\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = scanner.nextInt();
+        System.out.println();
+        if (userChoice == 0) {
+            System.out.println("See you next time!");
+        }
+        if (userChoice == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli cli = new Cli();
+            cli.getName();
+        }
+        if (userChoice == 2) {
+            Even.evenGame();
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,12 +1,16 @@
-package code;
+package hexlet.code;
 
 import java.util.Scanner;
 
 public class Cli {
+    private static String userName;
     public static void getName () {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name?");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
+    }
+    public static String getNameOnly() {
+        return userName;
     }
 }
