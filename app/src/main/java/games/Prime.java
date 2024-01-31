69f4ec709dfa84a35c2bac9873aacfb500b9375c
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-01-31 15:47:51 +0300

    игра- простое ли число

---
+++ app/src/main/java/games/Prime.java
@@ -0,0 +1,50 @@
+package games;
+
+import hexlet.code.Cli;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import static hexlet.code.Cli.getNameOnly;
+
+public class Prime {
+    private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        public static void gamePrime() {
+            System.out.println("Welcome to the Brain Games!");
+            Cli cli = new Cli();
+            cli.getName();
+            System.out.println(RULES);
+            int correctAnswers = 0;
+            Scanner sc = new Scanner(System.in);
+            Random rnd = new Random();
+            while (correctAnswers < 3) {
+                var randomNumber = rnd.nextInt(100);
+                System.out.println("Question: " + randomNumber);
+                System.out.print("Your answer: ");
+                String playersResponse = sc.next();
+                String correctAns = isPrime(randomNumber) ? "yes" : "no";
+                    if (playersResponse.equals(correctAns)) {
+                        System.out.println("Correct!");
+                        correctAnswers++;
+                    } else {
+                        System.out.println("'" + playersResponse + "' is wrong answer ;(. Correct answer was '" + correctAns + "'.");
+                        System.out.println("Let's try again, " + getNameOnly() + "!");
+                        break;
+                    }
+                }
+                sc.close();
+            }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
